dc3590ec06214772d24f82a502af5656516f2f63
navil-noor <navilnoor@gmail.com>
2026-09-07 18:41:08 +0200

    eslint debugging

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -10,109 +10,109 @@ const Person = require('./models/person')
 
 const morgan = require('morgan')
 
-morgan.token('body', (request, respond) => {
+morgan.token('body', (request) => {
   if (request.method === 'POST') {
     return JSON.stringify(request.body)
   } else {
-    return ""
+    return ''
   }
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello from Part 3!</h1>')
+  response.send('<h1>Hello from Part 3!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-        response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person =>{
+  Person.findById(request.params.id).then(person => {
     if (person) {
-        response.json(person)
+      response.json(person)
     } else {
-        response.status(404).end()
+      response.status(404).end()
     }
-    })
+  })
     .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
-    Person.countDocuments().then(count => {
-        response.send(`
+  Person.countDocuments().then(count => {
+    response.send(`
         <p>Phonebook has info for ${count} people</p>
         <p>${new Date().toString()}</p>
     `)
-    })
+  })
 })
 
 app.post('/api/persons', (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({error: 'data is missing'})
-    }
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'data is missing' })
+  }
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save()
+  person.save()
     .then(savedPerson => {
-        response.json(savedPerson)
+      response.json(savedPerson)
     })
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    Person.findById(request.params.id)
-        .then(person => {
-            if (!person) {
-                return response.status(404).end()
-            }
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
 
-            person.name = name
-            person.number = number
+      person.name = name
+      person.number = number
 
-            return person.save().then(updatedPerson => {
-                response.json(updatedPerson)
-            })
-        })
-        .catch(error => next(error))
+      return person.save().then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndDelete(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 // handler of requests with unknown endpoint
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 // handler of requests that result in errors
@@ -120,5 +120,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
